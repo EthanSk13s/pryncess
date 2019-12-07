b1f16cb0b58779d1820053c4ce5018b4c4bb8cb5
EthanSk13s <ethanmiranda11@gmail.com>
2019-12-06 19:10:54 -0800

    add version models

--- pryncess/models/versions.py
+++ pryncess/models/versions.py
@@ -4,9 +4,13 @@ class App(object):
         self.update = data['updateTime']
         self.revision = data['revision']
 
-# TODO: Inherit from App instead
 class Res(object):
     def __init__(self, data: dict):
         self.version = data['version']
         self.update = data['updateTime']
         self.index = data['indexName']
+
+class LatestVersion(object):
+    def __init__(self, data: dict):
+        self.app = App(data['app'])
+        self.res = Res(data['res'])
