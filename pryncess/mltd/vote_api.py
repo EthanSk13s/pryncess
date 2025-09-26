92ea767e5c67e6cadcad6edb3b21aa8044b349de
EthanSk13s <ethanmiranda11@gmail.com>
2025-09-26 09:42:07 -0700

    add docstrings to the voting api

--- pryncess/mltd/vote_api.py
+++ pryncess/mltd/vote_api.py
@@ -8,11 +8,40 @@ from pryncess.types.events import VotingEventDict
 
 
 class VoteAPI:
+    """Represents access to the voting data endpoint for the MLTD endpoint of the API.
+
+    This class serves as a wrapper around requests to the voting data API. Allowing
+    easy access and filtering for election and voting data throughout the game.
+
+    Example:
+
+    .. code-block:: py
+
+        from pryncess.mltd.mltd_client import MLTDClient
+
+        mltd = MLTDClient("ja")
+
+        vote_api = mltd.vote_api()
+        election = vote_api.get_votes(2)
+    """
     def __init__(self, session: requests.Session):
         self.prefix_url = f"/mltd/v2/ja/votes" # Only JP has voting events.
         self._client = Client(session)
     
     def get_votes(self, id: int | None = None) -> list[VotingEvent] | None:
+        """Fetches voting event data from the API.
+
+        Retrieves one or more elections based on the provided ID.
+
+        Args:
+            id (:class:`int`, optional): Specific election ID to fetch. Defaults to None. If None,
+                returns the entire list of existing elections.
+
+        Returns:
+            list[:class:`~pryncess.models.events.VotingEvent`] | None: A list of `VotingEvent` objects
+            if results are found, or None if the request fails.
+        """
+
         if id:
             resp = self._client.get(f"{self.prefix_url}/{id}")
         else:
@@ -31,6 +60,16 @@ class VoteAPI:
             return [VotingEvent(event)]
     
     def get_vote_logs(self, event: VotingEvent | int) -> list[VotingEventLogs] | None:
+        """Fetches voting event logs from the API.
+
+        Args:
+            event (:class:`~pryncess.models.events.VotingEvent` | :class:`int`): Specific election to fetch. Defaults to None.
+
+        Returns:
+            list[:class:`~pryncess.models.events.VotingEventLogs`] | None: A list of `VotingEventLogs` objects 
+            if results are found, or None if the request fails.
+        """
+
         resp = self._client.get(f"{self.prefix_url}/{int(event)}/rankings/logs")
 
         if not resp:
